fcb0352e943171da6919cbcca9db3ffda5907388
Fran Zekan <zekan.fran369@gmail.com>
2022-12-14 11:09:16 +0100

    Migrate typebox -> zod for easier usage

--- schemaGen.mjs
+++ schemaGen.mjs
@@ -4,54 +4,63 @@ import { join, dirname } from "node:path";
 import { fileURLToPath } from "node:url";
 import { writeFile } from "node:fs/promises";
 
-import { Type } from "@sinclair/typebox";
+import { z } from "zod";
+import { zodToJsonSchema } from "zod-to-json-schema";
 
-const StoreSchema = Type.Object({
-  id: Type.Integer(),
-  name: Type.String(),
-  description: Type.String(),
-  image: Type.String(),
-  address: Type.String(),
-  owner: Type.String(),
-  phoneNumber: Type.String(),
-  opensAt: Type.Integer(),
-  closesAt: Type.Integer(),
-  rating: Type.Number(),
-  createdAt: Type.String({ format: "date-time" }),
-  updatedAt: Type.String({ format: "date-time" }),
+const storeSchema = z.object({
+  id: z.number().int(),
+  name: z.string(),
+  description: z.string(),
+  image: z.string(),
+  address: z.string(),
+  owner: z.string(),
+  phoneNumber: z.string(),
+  opensAt: z.number().int(),
+  closesAt: z.number().int(),
+  rating: z.number(),
+  createdAt: z.date(),
+  updatedAt: z.date(),
 });
 
-const ProductSchema = Type.Object({
-  id: Type.Integer(),
-  name: Type.String(),
-  brand: Type.String(),
-  shortDescription: Type.String(),
-  description: Type.String(),
-  images: Type.Array(Type.String()),
-  price: Type.Integer(),
-  discount: Type.Integer(),
-  rating: Type.Integer(),
-  stock: Type.Integer(),
-  storeId: Type.Integer(),
-  createdAt: Type.String({ format: "date-time" }),
-  updatedAt: Type.String({ format: "date-time" }),
+const StoreSchema = zodToJsonSchema(storeSchema);
+
+const productSchema = z.object({
+  id: z.number().int(),
+  name: z.string(),
+  brand: z.string(),
+  shortDescription: z.string(),
+  description: z.string(),
+  images: z.array(z.string()),
+  price: z.number().int(),
+  discount: z.number().int(),
+  rating: z.number(),
+  stock: z.number().int(),
+  storeId: z.number().int(),
+  createdAt: z.date(),
+  updatedAt: z.date(),
 });
 
-const FullProductSchema = Type.Intersect([
-  ProductSchema,
-  Type.Object({
-    store: StoreSchema,
-  }),
-]);
+const ProductSchema = zodToJsonSchema(productSchema);
+
+const fullProductSchema = productSchema.extend({
+  store: storeSchema,
+});
+
+const FullProductSchema = zodToJsonSchema(fullProductSchema, { strictUnions: true });
 
 await Promise.all([
   writeFile(
-    join(dirname(fileURLToPath(import.meta.url)), "./public/productSchema.json"),
+    join(dirname(fileURLToPath(import.meta.url)), "./public/schema/product.json"),
     JSON.stringify(ProductSchema, null, 2)
   ),
 
   writeFile(
-    join(dirname(fileURLToPath(import.meta.url)), "./public/fullProductSchema.json"),
+    join(dirname(fileURLToPath(import.meta.url)), "./public/schema/store.json"),
+    JSON.stringify(StoreSchema, null, 2)
+  ),
+
+  writeFile(
+    join(dirname(fileURLToPath(import.meta.url)), "./public/schema/fullProduct.json"),
     JSON.stringify(FullProductSchema, null, 2)
   ),
 ]);
