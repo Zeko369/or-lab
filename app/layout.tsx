5edb841553f1827b0e736793bdf6071961c46b75
Fran Zekan <zekan.fran369@gmail.com>
2023-01-07 20:53:15 +0100

    Fix warnings

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,14 +4,14 @@ import { getServerSession } from "~/lib/auth";
 import "./globals.css";
 
 export default async function RootLayout({ children }: { children: React.ReactNode }) {
-  const user = await getServerSession();
+  const session = await getServerSession();
 
   return (
     <html lang="en">
       <head />
       <body>
         <div className="p-2">
-          <ClientProviders user={user}>{children}</ClientProviders>
+          <ClientProviders session={{ ...session } as any}>{children}</ClientProviders>
         </div>
       </body>
     </html>
--- components/Providers.tsx
+++ components/Providers.tsx
@@ -5,13 +5,13 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { UserProvider } from "@auth0/nextjs-auth0/client";
 import { Session } from "@auth0/nextjs-auth0";
 
-type Props = PropsWithChildren<{ user: Session | null }>;
+type Props = PropsWithChildren<{ session: Session | null }>;
 
-export const ClientProviders: React.FC<Props> = ({ children, user }) => {
+export const ClientProviders: React.FC<Props> = ({ children, session }) => {
   const [queryClient] = useState(() => new QueryClient());
 
   return (
-    <UserProvider user={user?.user}>
+    <UserProvider user={session?.user}>
       <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
     </UserProvider>
   );
